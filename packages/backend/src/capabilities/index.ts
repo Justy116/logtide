d40f6835b4bd8d5b2cd70b468cb1dbb2336f9ca5
Polliog <polliogiuseppe@live.it>
2026-06-06 01:39:25 +0200

    add capabilities barrel

---
+++ packages/backend/src/capabilities/index.ts
@@ -0,0 +1,34 @@
+export {
+  CAPABILITIES,
+  CAPABILITY_NAMES,
+  isBooleanCapability,
+  isLimitOrQuotaCapability,
+  isQuotaCapability,
+  isKnownCapability,
+  type CapabilityName,
+  type CapabilityDef,
+  type QuotaWindow,
+} from './registry.js';
+
+export {
+  DbCapabilityResolver,
+  type CapabilityResolver,
+  type EntitlementValue,
+} from './resolver.js';
+
+export {
+  capabilities,
+  setCapabilityResolver,
+  getCapabilityResolver,
+} from './facade.js';
+
+export { CapabilityError, QuotaExceededError } from './errors.js';
+
+export {
+  assertCapability,
+  assertWithinLimit,
+  assertWithinUsageQuota,
+} from './enforce.js';
+
+export { quotaFlagCache } from './quota-cache.js';
+export { QuotaEvaluator } from './quota-evaluator.js';
